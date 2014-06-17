500a33c3798a57bb5ed746f02b69ac98f1887a50
simogrima <simogrima@gmail.com>
2014-06-17 10:17:30 +0200

    prima di nuovo layout

--- config/application.config.php
+++ config/application.config.php
@@ -22,7 +22,7 @@ return array(
         'User',
         'MyZfcRbac',
         'Admin',
-        'Samples'
+        'Samples' //campionature
         ),
     'module_listener_options' => array(
         'module_paths' => array(
